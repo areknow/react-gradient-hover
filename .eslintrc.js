df858fb95dfebc8e27cb7cbf5c0cdb8c0465c6b1
Arnaud Crowther <arnaudcrowther@gmail.com>
2025-07-01 12:29:41 -0400

    remove vite

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  ignorePatterns: ["vite.config.js", "rollup.config.js"],
+  ignorePatterns: ["rollup.config.js"],
   overrides: [
     {
       files: ["*.ts", "*.tsx"],
